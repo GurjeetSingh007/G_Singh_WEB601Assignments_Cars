a44af405bf24bf899bf90e1df2ad9bd60b7aef6d
GurjeetSingh007 <w0792908@myscc.ca>
2023-02-20 21:32:59 -0500

    Added default image

--- G_Singh_Cars/src/app/content-list/content-list.component.ts
+++ G_Singh_Cars/src/app/content-list/content-list.component.ts
@@ -10,6 +10,7 @@ export class ContentListComponent {
   cards: Content[];
   titleValue: string = '';
   isTitleFound: boolean | null = null;
+  img: string ='https://preview.redd.it/9r1ytuzin9g61.png?width=960&crop=smart&auto=webp&v=enabled&s=4dc692d6a006cabfbb88793afd52e25e4332bbd6';
 
   titleSearch(){
     this.isTitleFound = this.cards.some(card => card.title.toLowerCase() === this.titleValue.toLowerCase());
