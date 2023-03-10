9432cce06c4b3a6cf8da2874f69adf7e46b10726
GurjeetSingh007 <w0792908@myscc.ca>
2023-03-10 18:52:58 -0500

    Added html tags and functions with css

--- G_Singh_Cars/src/app/app.component.ts
+++ G_Singh_Cars/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CarsService } from './services/cars.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'G_Singh_Cars';
+  contentId: number = 1;
+  topContent: any = {}; 
+  img: string ='https://preview.redd.it/9r1ytuzin9g61.png?width=960&crop=smart&auto=webp&v=enabled&s=4dc692d6a006cabfbb88793afd52e25e4332bbd6';
+
+constructor(private CarsService: CarsService) {}
+
+  log(){
+    console.log(`ID:${this.topContent.id}`);
+    console.log(`Title: ${this.topContent.title}`);
+  }
+
+  ngOnInit(){
+    this.CarsService.getContentById(this.contentId).subscribe
+    (content => this.topContent = content);
+  }
 }
