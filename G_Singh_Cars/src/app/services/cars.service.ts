125400339e05a48c527c582522f93337b6a3270a
GurjeetSingh007 <w0792908@myscc.ca>
2023-03-10 19:17:25 -0500

    add  message and get functionalty

--- G_Singh_Cars/src/app/services/cars.service.ts
+++ G_Singh_Cars/src/app/services/cars.service.ts
@@ -2,21 +2,24 @@ import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { Content } from '../helper-files/content-interface';
 import { cardsArray } from '../helper-files/contentDb';
+import { MessageService } from './message.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CarsService {
 
-  constructor() { }
+  constructor(private messageService: MessageService) { }
 
 
   getContent(): Observable<Content[]>{
-       return of(cardsArray);
+    this.messageService.addMessage("Content array loaded!");
+    return of(cardsArray);
   }
 
   getContentById(id: number): Observable<any> {
     const content = cardsArray.find(c => c.id === id);
-      return of(content);
+    this.messageService.addMessage(`Content item at id: ${id}`);
+    return of(content);
   }
 }
